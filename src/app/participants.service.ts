24060fd80a828d3aef862236849e1bc1bea79333
Ahmed Taj elsir <ah.tajelsir@gmail.com>
2017-02-07 15:48:57 +0300

    Added missing participants

--- src/app/participants.service.ts
+++ src/app/participants.service.ts
@@ -5,31 +5,18 @@ import 'rxjs/add/operator/map';
 
 import * as $ from 'jquery';
 
+const participants = require('./participants.json');
+
 import { Participant } from './participant';
 
 
 @Injectable()
 export class ParticipantsService {
-  participants = [
-    { 'name': 'Trteel', 'total_cont': 0, 'username': 'Tarteel' },
-    { 'name': 'Amr Abd Alkrim Hassan', 'total_cont': 2, 'username': 'wizmoori' },
-    { 'name': 'Ameenah', 'total_cont': 0, 'username': 'ameenah' },
-    { 'name': 'Majid CJ', 'total_cont': 0, 'username': 'majid-cj' },
-    { 'name': 'Obay Hamed', 'total_cont': 0, 'username': 'obayhamed' },
-    { 'name': 'Ibrahim Omer', 'total_cont': 1, 'username': 'ibrahimomer' },
-    { 'name': 'Montaser', 'total_cont': 0, 'username': 'montaz7' },
-    { 'name': 'Abdalmajid Abdalla', 'total_cont': 0, 'username': 'majidsd' },
-    { 'name': 'Eman saeed', 'total_cont': 0, 'username': 'emansaeed' },
-    { 'name': 'wiamosman', 'total_cont': 0, 'username': 'wiamosman' },
-    { 'name': 'emanjodoo', 'total_cont': 0, 'username': 'emanjodoo' },
-    { 'name': 'mohannad waheed', 'total_cont': 14, 'username': 'mohannadprogrammer' },
-    { 'name': 'ProjectDeveloper323', 'total_cont': 0, 'username': 'ProjectDeveloper323' }
-  ];
 
   constructor(private http: Http) { }
 
   getParticipants(): Promise<Participant[]> {
-    return Promise.resolve(this.participants.sort(this.compare));
+    return Promise.resolve(participants.sort(this.compare));
   }
   compare(a, b) {
     if (a.total_cont > b.total_cont) {
@@ -37,6 +24,11 @@ export class ParticipantsService {
     } else if (a.total_cont < b.total_cont) {
       return 1;
     } else {
+      if (a.name < b.name) {
+        return -1;
+      } else if (a.name > b.name) {
+        return 1;
+      }
       return 0;
     }
   }
